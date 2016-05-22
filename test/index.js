559166b03e447593411f7f4168800ea7d23728d9
Fengda HUANG <h@phodal.com>
2016-05-23 00:23:42 +0800

    init projects

---
+++ test/index.js
@@ -0,0 +1,8 @@
+import assert from 'assert';
+import solutions from '../lib';
+
+describe('solutions', function () {
+  it('should have unit test!', function () {
+    assert(false, 'we expected this package author to add actual unit tests.');
+  });
+});
