5d2b73b723de9ac2ab34cd91ef91d85b4f394688
Fengda HUANG <h@phodal.com>
2016-05-25 09:11:46 +0800

    add commander for cli

--- bin/solutions.js
+++ bin/solutions.js
@@ -1,4 +1,14 @@
 #!/usr/bin/env node
 var solutions = require('../dist/');
+var program = require('commander');
+var pjson = require('../package.json');
 
-solutions.generate('dist', 2);
+program
+  .version(pjson.version)
+  .option('-p, --path', 'set JSON files path')
+  .option('-s, --size', 'set each JSON file size')
+  .parse(process.argv);
+gi
+if (program.path && program.size) {
+  solutions.generate(program.path, program.size);
+}
